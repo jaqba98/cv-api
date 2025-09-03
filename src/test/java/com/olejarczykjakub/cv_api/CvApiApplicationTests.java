9433475fd2b9d8511e700352cc8d5abd16d5037b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-03 09:44:09 +0200

    Init the cv api project

---
+++ src/test/java/com/olejarczykjakub/cv_api/CvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olejarczykjakub.cv_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
