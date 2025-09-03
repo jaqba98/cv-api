f7e5affee95beee35509c013abf93d3e541f6d79
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-03 12:31:31 +0200

    Change the endpoint return to string in order to display the html page

--- src/main/java/com/olejarczykjakub/cv_api/controller/TechnologyController.java
+++ src/main/java/com/olejarczykjakub/cv_api/controller/TechnologyController.java
@@ -1,7 +1,6 @@
 package com.olejarczykjakub.cv_api.controller;
 
 import com.olejarczykjakub.cv_api.entity.Technology;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +9,10 @@ import java.util.ArrayList;
 @RestController
 public class TechnologyController {
     @GetMapping("/technology")
-    public ResponseEntity<ArrayList<Technology>> getTechnology() {
+    public String getTechnology() {
         var technology = new ArrayList<Technology>();
         technology.add(new Technology("xml", "be"));
-        return ResponseEntity.ok().body(technology);
+        System.out.println(technology);
+        return "Technology works!";
     }
 }
