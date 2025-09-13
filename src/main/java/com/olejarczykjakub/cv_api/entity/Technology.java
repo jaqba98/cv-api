83e16149dc64c04f596305baebf90db871a6dfd3
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-13 23:57:25 +0200

    Add lombok and update the entity technology

--- src/main/java/com/olejarczykjakub/cv_api/entity/Technology.java
+++ src/main/java/com/olejarczykjakub/cv_api/entity/Technology.java
@@ -1,41 +1,22 @@
 package com.olejarczykjakub.cv_api.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Technology {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "technology_id")
-    private Long technologyId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  // todo: Set up the @Column here because now there is only name
+  @Column(name = "technology_id")
+  private Long technologyId;
 
-    @Column(name = "technology_name")
-    private String technologyName;
+  // todo: Set up the @Column here because now there is only name
+  @Column(name = "technology_name")
+  private String technologyName;
 
-    @Column(name = "technology_group")
-    private String technologyGroup;
-
-    protected Technology() {
-    }
-
-    public Technology(String technologyName, String technologyGroup) {
-        this.technologyName = technologyName;
-        this.technologyGroup = technologyGroup;
-    }
-
-    public String getTechnologyName() {
-        return technologyName;
-    }
-
-    public void setTechnologyName(String technologyName) {
-        this.technologyName = technologyName;
-    }
-
-    public String getTechnologyGroup() {
-        return technologyGroup;
-    }
-
-    public void setTechnologyGroup(String technologyGroup) {
-        this.technologyGroup = technologyGroup;
-    }
+  // todo: Set up the @Column here because now there is only name
+  @Column(name = "technology_group")
+  private String technologyGroup;
 }
