8d895dbd68a588f5649c3ca402fa7642df6fbd0f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-13 19:43:35 +0200

    Create technology repository

---
+++ src/main/java/com/olejarczykjakub/cv_api/repository/TechnologyRepository.java
@@ -0,0 +1,7 @@
+package com.olejarczykjakub.cv_api.repository;
+
+import com.olejarczykjakub.cv_api.entity.Technology;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TechnologyRepository extends CrudRepository<Technology, Long> {
+}
