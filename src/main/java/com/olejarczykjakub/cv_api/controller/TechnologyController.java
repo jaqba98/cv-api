4453a5292a271909ead08cdb6089ad7f5b7583d7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-17 04:47:53 +0200

    Return the technology groups as a response

--- src/main/java/com/olejarczykjakub/cv_api/controller/TechnologyController.java
+++ src/main/java/com/olejarczykjakub/cv_api/controller/TechnologyController.java
@@ -1,11 +1,14 @@
 package com.olejarczykjakub.cv_api.controller;
 
+import com.olejarczykjakub.cv_api.entity.TechnologyGroup;
 import com.olejarczykjakub.cv_api.service.TechnologyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +17,7 @@ public class TechnologyController {
   private final TechnologyService service;
 
   @GetMapping("/get-all")
-  public ResponseEntity<String> getAll() {
+  public ResponseEntity<ArrayList<TechnologyGroup>> getAll() {
     return ResponseEntity.ok().body(service.getAll());
   }
 }
--- src/main/java/com/olejarczykjakub/cv_api/service/TechnologyService.java
+++ src/main/java/com/olejarczykjakub/cv_api/service/TechnologyService.java
@@ -1,11 +1,14 @@
 package com.olejarczykjakub.cv_api.service;
 
+import com.olejarczykjakub.cv_api.entity.TechnologyGroup;
 import com.olejarczykjakub.cv_api.repository.TechnologyCategoryRepository;
 import com.olejarczykjakub.cv_api.repository.TechnologyGroupRepository;
 import com.olejarczykjakub.cv_api.repository.TechnologyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +17,10 @@ public class TechnologyService {
   private final TechnologyGroupRepository technologyGroupRepository;
   private final TechnologyCategoryRepository technologyCategoryRepository;
 
-  public String getAll() {
+  public ArrayList<TechnologyGroup> getAll() {
     val technologyGroup = technologyGroupRepository.findAll();
     val technologyCategory = technologyCategoryRepository.findAll();
     val technology = technologyRepository.findAll();
-    System.out.println(technologyGroup);
-    System.out.println();
-    System.out.println(technologyCategory);
-    System.out.println();
-    System.out.println(technology);
-    return "getAll()";
+    return (ArrayList<TechnologyGroup>) technologyGroup;
   }
 }
